8813b849632d673a4c8a50e0bfaffc8052db6a54
Alejandra Salgado <malejandrasalgado@gmail.com>
2021-09-15 13:17:31 +1000

    adding modificationes

--- Leaflet/static/js/logic.js
+++ Leaflet/static/js/logic.js
@@ -1,20 +1,14 @@
 // Create the tiles layer that will be the background of our map
 
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>,\
     Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "light-v10",
+    id: "dark-v10",
     accessToken: API_KEY
 });
 
-var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>,\
-    Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "satellite-v9",
-    accessToken: API_KEY
-});
+
 
 var outdoorsmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>,\
@@ -33,8 +27,8 @@ var layers = {
 // Create a baseMaps objects to hold the Layers
 
 var baseMaps = {
-    "Satellite": satellitemap,
-    "Grayscale": lightmap,
+    
+    "Darkscale": darkmap,
     "Outdoors": outdoorsmap
 };
 
@@ -49,7 +43,7 @@ var overlayMaps = {
 var map = L.map("map", {
     center: [-37.81523132324219,144.96360778808594],
     zoom: 15    ,
-    layers: [lightmap, satellitemap, outdoorsmap]
+    layers: [darkmap, outdoorsmap]
 });
 
 // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
